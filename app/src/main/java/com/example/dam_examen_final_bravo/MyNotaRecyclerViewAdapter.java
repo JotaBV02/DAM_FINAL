980214e6d38552debdaa1b75f31b5a21b7f39eb9
Jorge Bravo <jbravoveintemilla@gmail.com>
2024-12-16 16:19:10 -0500

    Commit 02-Bravo: Notas en Columnas

---
+++ app/src/main/java/com/example/dam_examen_final_bravo/MyNotaRecyclerViewAdapter.java
@@ -0,0 +1,79 @@
+package com.example.dam_examen_final_bravo;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.example.dam_examen_final_bravo.databinding.FragmentItemBinding;
+
+import java.util.List;
+
+
+public class MyNotaRecyclerViewAdapter extends RecyclerView.Adapter<MyNotaRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Notas> mValues;
+    private final NotasInteractionListener mListener;
+
+
+    public MyNotaRecyclerViewAdapter(List<Notas> notaList, NotasInteractionListener mListener) {
+        this.mValues = notaList;
+        this.mListener = mListener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+
+                .inflate(R.layout.fragment_item, parent, false);
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.tvTitulo.setText(holder.mItem.getTitulo());
+        holder.tvContenido.setText(holder.mItem.getContenido());
+        if(holder.mItem.isFavorita()) {
+            holder.ivFavorita.setImageResource(R.drawable.estrella);
+        }
+        holder.ivFavorita.setOnClickListener(v->{
+            if(null != mListener){
+                mListener.favoritaNotaClick(holder.mItem);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView tvTitulo;
+        public final TextView tvContenido;
+        public final ImageView ivFavorita;
+        public Notas mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            tvTitulo = view.findViewById(R.id.textViewTitulo);
+            tvContenido = view.findViewById(R.id.textViewContenido);
+            ivFavorita = view.findViewById(R.id.imageViewFavorita);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + tvTitulo.getText() + "'";
+        }
+    }
+}
---
+++ app/src/main/java/com/example/dam_examen_final_bravo/NotasInteractionListener.java
@@ -0,0 +1,7 @@
+package com.example.dam_examen_final_bravo;
+
+public interface NotasInteractionListener {
+    void editNotaClick(Notas nota);
+    void eliminaNotaClick (Notas nota);
+    void favoritaNotaClick (Notas nota);
+}
